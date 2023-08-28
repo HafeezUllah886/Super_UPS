bb18d762f517bd48c64d74b7b72b3870f95dfd53
HafeezUllah886 <Hafeezullah886@gmail.com>
2023-08-28 17:02:26 +0500

    rebased

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-        <title>Super UPS Center</title>
+        <title>Insaf Electric Store</title>
         <meta content="Admin Dashboard" name="description" />
         <meta content="Mannatthemes" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -16,9 +16,9 @@
         <link href= {{ asset("assets/plugins/jvectormap/jquery-jvectormap-2.0.2.css") }} rel="stylesheet">
 
         <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-<script defer src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-<script defer src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
-<script defer src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
+        <script defer src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+        <script defer src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+        <script defer src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
 
 
         <link href= {{ asset("assets/css/bootstrap.min.css") }} rel="stylesheet" type="text/css">
@@ -38,7 +38,7 @@
           body {
             font-family: "Noto Nastaliq Urdu";
             font-weight: 900;
-  
+
           }
           </style>
         @endif
