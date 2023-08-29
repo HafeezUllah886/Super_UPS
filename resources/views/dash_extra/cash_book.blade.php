b0bca3697490ac320d62ff39930d3d79bccf3b68
HafeezUllah886 <Hafeezullah886@gmail.com>
2023-08-29 15:26:49 +0500

    Cashbook Updated

--- resources/views/dash_extra/cash_book.blade.php
+++ resources/views/dash_extra/cash_book.blade.php
@@ -29,7 +29,7 @@
                             <th>{{ __('lang.Ser') }}</th>
                             <th>{{ __('lang.Account') }}</th>
                             <th>{{ __('lang.Desc') }}</th>
-                            {{-- <th>{{ __('lang.Details') }}</th> --}}
+                            <th>{{ __('lang.Customer') }}</th>
                             <th>{{ __('lang.Amount') }}</th>
                         </thead>
                         <tbody>
@@ -46,92 +46,28 @@
                                     <td>{{ $trans->account->title }}</td>
 
                                     <td>{!! $trans->desc !!}</td>
-                                    {{-- <td>
+                                    <td>
                                         @if ($trans->type == 'Sale')
                                         @php
-                                            $data = \App\Models\sale_details::with('product')->where('ref', $trans->ref)->get();
-                                            $subTotal = 0;
+                                            $data = \App\Models\sale::with('customer_account')->where('ref', $trans->ref)->first();
+                                            if (@$data->walking)
+                                            {
+                                                echo $data->walking." (Walk-in)"; 
+                                                 
+                                            }
+                                            else
+                                            {
+                                                echo @$data->customer_account->title;
+                                            }
+                                            
                                         @endphp
-                                        <table class="table">
-                                            <th>{{ __('lang.Product') }}</th>
-                                            <th>{{ __('lang.Size') }}</th>
-                                            <th>{{ __('lang.Qty') }}</th>
-                                            <th>{{ __('lang.Price') }}</th>
-                                            <th>{{ __('lang.Amount') }}</th>
-                                            @foreach ($data as $data1)
-                                            @php
-                                                $subTotal = $data1->qty * $data1->price;
-                                            @endphp
-                                                <tr>
-                                                    <td>{{$data1->product->name}}</td>
-                                                    <td>{{$data1->product->size}}</td>
-                                                    <td>{{$data1->qty}}</td>
-                                                    <td>{{round($data1->price,0)}}</td>
-                                                    <td>{{$subTotal}}</td>
-                                                </tr>
-                                            @endforeach
-
-                                        </table>
-                                        <strong>{{ __('lang.Discount') }}: </strong>{{$data[0]->bill->discount ?? '0'}}
-                                        @endif
-                                        @if ($trans->type == 'Purchase')
-                                        @php
-                                        $data = \App\Models\purchase_details::with('product')->where('ref', $trans->ref)->get();
-                                        $subTotal = 0;
-                                        @endphp
-                                        <table class="table">
-                                            <th>{{ __('lang.Product') }}</th>
-                                            <th>{{ __('lang.Size') }}</th>
-                                            <th>{{ __('lang.Qty') }}</th>
-                                            <th>{{ __('lang.Price') }}</th>
-                                            <th>{{ __('lang.Amount') }}</th>
-                                        @foreach ($data as $data1)
-                                        @php
-                                            $subTotal = $data1->qty * $data1->rate;
-                                        @endphp
-                                            <tr>
-                                                <td>{{$data1->product->name}}</td>
-                                                <td>{{$data1->product->size}}</td>
-                                                <td>{{$data1->qty}}</td>
-                                                <td>{{round($data1->rate,2)}}</td>
-                                                <td>{{$subTotal}}</td>
-                                            </tr>
-                                        @endforeach
-
-                                        </table>
-                                        @endif
-                                        @if ($trans->type == 'Sale Return')
-                                        @php
-                                        $data = \App\Models\saleReturnDetails::with('product')->where('ref', $trans->ref)->get();
-                                        $subTotal = 0;
-                                        @endphp
-                                        <table class="table">
-                                            <th>{{ __('lang.Product') }}</th>
-                                            <th>{{ __('lang.Size') }}</th>
-                                            <th>{{ __('lang.Qty') }}</th>
-                                            <th>{{ __('lang.Price') }}</th>
-                                            <th>{{ __('lang.Amount') }}</th>
-                                        @foreach ($data as $data1)
-                                        @php
-                                        $subTotal = $data1->qty * $data1->price;
-                                        @endphp
-                                        <tr>
-                                            <td>{{$data1->product->name}}</td>
-                                            <td>{{$data1->product->Size}}</td>
-                                            <td>{{$data1->qty}}</td>
-                                            <td>{{round($data1->price,2)}}</td>
-                                            <td>{{$subTotal}}</td>
-                                        </tr>
-                                        @endforeach
-
-                                        </table>
-                                        @endif
-                                    </td> --}}
+                                       @endif
+                                    </td>
                                     <td>{{ round($trans->cr,0) }}</td>
                                 </tr>
                             @endforeach
                             <tr class="th-color">
-                                <td colspan="3" class="text-right">{{ __('lang.Total') }}: &nbsp;
+                                <td colspan="4" class="text-right">{{ __('lang.Total') }}: &nbsp;
                                 </td>
                                 <td>{{ $total_cr }}</td>
                             </tr>
