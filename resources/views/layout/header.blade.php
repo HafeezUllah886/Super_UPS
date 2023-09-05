8813af0665ec0dde91d5793d4ebaac79da7f9347
HafeezUllah886 <Hafeezullah886@gmail.com>
2023-09-05 17:27:42 +0500

    name changed

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-        <title>Super UPS Center</title>
+        <title>POS</title>
         <meta content="Admin Dashboard" name="description" />
         <meta content="Mannatthemes" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -38,7 +38,7 @@
           body {
             font-family: "Noto Nastaliq Urdu";
             font-weight: 900;
-  
+
           }
           </style>
         @endif
