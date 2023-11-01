190f222f32d9206c3c2f3983afad446a43503495
HafeezUllah886 <Hafeezullah886@gmail.com>
2023-11-01 16:44:40 +0500

    Header added

--- resources/views/sale/print.blade.php
+++ resources/views/sale/print.blade.php
@@ -251,20 +251,7 @@
 <body>
 
     <div class="container">
-        <div class="container1">
-            <div class="sub-container">
-                <div class="logo" style="width: 37%;">
-                    <img src="{{ asset('assets/images/app_logo.png') }}" alt="logo">
-                </div>
-              
-                <div class="text1">
-                    <h1 class="m-query1">POS</h1>
-                    <h3 class="m-query2">Shop No 12, insaf Solar Market, Angle Road, opp Civic Center, Quetta.
-                        <br>Phone:&nbsp; 0300-3883054,&nbsp;0309-8105556,&nbsp;081-2827774</h3>
-                </div>
-            </div>
-        </div>
-
+        <img style="margin:0;width:100%;" src="{{ asset('assets/images/bill.png') }}" alt="">
         <div class="body-section">
             <div class="row">
                 <div class="qoute">
@@ -391,7 +378,7 @@
                 </tbody>
             </table>
             <br>
-            <table style="width:500px;">
+            {{-- <table style="width:500px;">
                 <tr>
                     <td style="text-align: left; width:40%;"> <strong>Payment type:</strong> </td>
                     <td style="text-align: left">{{$invoice->account->title ?? "Unpaid"}}</td>
@@ -414,11 +401,11 @@
                     <td style="text-align: left">{{ $cur_balance }}</td>
                 </tr>
                 @endif
-            </table>
+            </table> --}}
             <div class="d-flex justify-content-end">
                 <br><br>
                 <h4 style="text-align:right;margin-right:2px;">Authorize Signature___________________</h4>
-                <p style="margin-right:2px;">superupscenter@gmail.com</p>
+               {{--  <p style="margin-right:2px;">superupscenter@gmail.com</p> --}}
                 <br>
             </div>
 
