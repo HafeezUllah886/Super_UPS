4d0abb81bd51c2a510de21dbed71b2703de314f0
HafeezUllah886 <Hafeezullah886@gmail.com>
2023-11-27 19:50:09 +0500

    working on scrap

---
+++ database/migrations/2023_11_27_173147_create_scrap_sales_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('scrap_sales', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->string("customerName")->default("Walkin Customer");
+            $table->float('weight', 10,2);
+            $table->float("rate", 10);
+            $table->text("desc")->nullable();
+            $table->integer("ref");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('scrap_sales');
+    }
+};
