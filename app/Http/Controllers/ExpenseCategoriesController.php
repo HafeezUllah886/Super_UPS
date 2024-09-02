386fcb06342360ac414dc914bb5d5da3f9ea9ed5
HafeezUllah886 <55585864+HafeezUllah886@users.noreply.github.com>
2024-09-02 20:16:20 +0500

    working on exp

---
+++ app/Http/Controllers/ExpenseCategoriesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\expense_categories;
+use Illuminate\Http\Request;
+
+class ExpenseCategoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(expense_categories $expense_categories)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(expense_categories $expense_categories)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, expense_categories $expense_categories)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(expense_categories $expense_categories)
+    {
+        //
+    }
+}
--- database/migrations/2023_06_11_092941_create_expenses_table.php
+++ database/migrations/2023_06_11_092941_create_expenses_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('expenses', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('account_id');
+            $table->foreignId('catID')->constrained('expenses', 'id');
             $table->timestamp('date', $precision = 0);
             $table->text('desc')->nullable();
             $table->unsignedDecimal('amount', 10, 2);
