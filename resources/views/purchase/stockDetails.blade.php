6bb3b9f4f6e2d8ec03742fd777a5ae664881749f
HafeezUllah886 <Hafeezullah886@gmail.com>
2025-01-15 22:15:42 +0500

    oho

--- resources/views/purchase/stockDetails.blade.php
+++ resources/views/purchase/stockDetails.blade.php
@@ -123,7 +123,7 @@
         var from = $('#from').val();
         var to = $('#to').val();
 
-    window.open("{{ url('/stock/details/') }}/"+{{ $product_id }}+"/"+from+"/"+to, '_self');
+    window.open("{{ url('/stock/details/') }}/"+{{ $product->id }}+"/"+from+"/"+to, '_self');
     }
 
 
